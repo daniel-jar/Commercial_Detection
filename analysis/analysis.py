432f90a8981e3a54326efb7239c2630e593d2980
Daniel Jarocha <daniel@jarocha.de>
2022-07-26 02:34:31 +0200

    finished scatter

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -1,4 +1,6 @@
+from re import X
 from tkinter import Y
+from turtle import xcor
 from async_generator import yield_from_
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -14,44 +16,11 @@ from matplotlib.patches import Patch
 import matplotlib.patches as mpatches
 from os import listdir
 
-#graveyard
-#df["Zeit"] = pd.to_datetime(df["Zeit"]).dt.time
-#print(df.head(1))
-# dfWerbung = specificDataFrame[df['LABEL'] == "Werbung"]
-# dfProgramm = specificDataFrame[df['LABEL'] == "Programm"]
-#columnArray=[5,6,7,8,9,10,11,12,13,16,17]
-#print(os.getcwd())
-# #def createSumHistograms():
-#     fig, axes = plt.subplots(len(columnArray)//3, 4, figsize=(12, 48))
-
-#     i = 0
-#     for triaxis in axes:
-#         for axis in triaxis:
-#             if i!=11:
-#                 if i==10:
-#                     minute_locator = mdates.MinuteLocator(interval=240)
-#                     hour_locator = mdates.HourLocator(interval=6)
-#                     axis.xaxis.set_major_locator(minute_locator)
-#                     myFmt = mdates.DateFormatter('%HH')
-#                     axis.xaxis.set_major_formatter(myFmt)
-#                     specificDataFrame.hist(column = specificDataFrame.columns[i], bins = 100, ax=axis) # Locator for major axis only.)
-#                 else:
-#                     specificDataFrame.hist(column = specificDataFrame.columns[i], bins = 100, ax=axis)
-#                 i = i+1
-        
-#     plt.show()
-def find_csv_filenames( path_to_dir, suffix=".csv" ):
-    filenames = listdir(path_to_dir)
-    return [ PATH_DATA_TO_BE_JOINED+"\\"+filename for filename in filenames if filename.endswith( suffix ) ]
-
-def returnJoinedDataFrame(files):
-    # list of merged files returned
-    files = glob.glob(files)
-    print("Result CSV after joining all CSV files at a particular location...");
-    # joining files with concat and read_csv
-    df = pd.concat(map(pd.read_csv, files), ignore_index=True)
-    print(df)
-    return df
+PATH_DATA_TO_BE_JOINED = os.getcwd()+"\\analysis\\input"
+COLOR_FOR_WERBUNG = "orange"
+COLOR_FOR_PROGRAMM ="cornflowerblue"
+STATUSES=[["Programm",COLOR_FOR_PROGRAMM],["Werbung",COLOR_FOR_WERBUNG]]
+ALPHA_VAL = 0.3
 
 def returnJoinedDataFrame(path):
     #join data paths
@@ -64,17 +33,6 @@ def returnJoinedDataFrame(path):
     df = pd.concat(map(pd.read_csv, files), ignore_index=True)
     print(df)
     return df
-
-PATH_DATA_TO_BE_JOINED = os.getcwd()+"\\analysis\\input"
-COLOR_FOR_WERBUNG = "orange"
-COLOR_FOR_PROGRAMM ="cornflowerblue"
-STATUSES=[["Programm",COLOR_FOR_PROGRAMM],["Werbung",COLOR_FOR_WERBUNG]]
-
-#files = find_csv_filenames(PATH_DATA_TO_BE_JOINED, suffix=".csv" )
-df = returnJoinedDataFrame(PATH_DATA_TO_BE_JOINED)
-df["Zeit"]=df["Zeit"].astype("datetime64[ns]")
-columnArray=["ECR_RATIO","MVL SUM","MVL ABS","RMS","DB","ZCR","MFCC","FARBWECHSEL RATIO","SIFT RATIO","Tag","Zeit","LABEL"]
-specificDataFrame = df[columnArray]
 
 def setTextinPlot(graph,values):
     i=0
@@ -111,7 +69,7 @@ def createCount(countDatapointsProgramm,countDatapointsWerbung):
     percentage = [percentageProgramm,percentageWerbung]
     return [AnzahlDerDatenPunkte,percentage]
    
-def createSumWerbungProgramHistograms(arrDf,STATUSES):
+def createSumWerbungProgramHistograms(arrDf,STATUSES,columnArray):
     fig, axes = plt.subplots(len(columnArray)//4, 4, figsize=(12, 48))
     countOfData = 0
     for x in STATUSES:
@@ -131,7 +89,6 @@ def createSumWerbungProgramHistograms(arrDf,STATUSES):
                             specificDataFrame.hist(column = specificDataFrame.columns[counterForPlots],color = STATUSES[countOfData][1], bins = 100, ax=axis,label=x)
                         else:
                             specificDataFrame.hist(column = specificDataFrame.columns[counterForPlots],color = STATUSES[countOfData][1], bins = 100, ax=axis)
-
                     elif counterForPlots == 11 and countOfData==1:
                         certainCounts = createCount(len(arrDf[0]),len(arrDf[1]))
                         countPlot = plt.bar(["Programm","Werbung"],certainCounts[0],color=[COLOR_FOR_PROGRAMM, COLOR_FOR_WERBUNG])
@@ -140,14 +97,10 @@ def createSumWerbungProgramHistograms(arrDf,STATUSES):
                         plt.title("Anzahl der Datenpunkte")
                     counterForPlots +=1
         countOfData+=1  
-    fig.legend(loc='upper center')    
-    plt.show()
-
-def createHeatmap(heatmap):
-    ##createHeatmap(specificDataFrame.corr())
-    plt.figure(figsize=(13, 6))
-    sns.heatmap(heatmap, vmax=1, annot=True, linewidths=.5)
-    plt.xticks(rotation=30, horizontalalignment="right")
+    manager=plt.get_current_fig_manager()
+    manager.full_screen_toggle()    
+    fig.legend(loc='upper left')
+    fig.suptitle("Histogramme für alle Merkmale", fontsize=14)    
     plt.show()
 
 def removeOutliers(specificDataFrame):
@@ -180,33 +133,12 @@ def removeOutliers(specificDataFrame):
     print(specificDataFrame.describe())
     return specificDataFrame
 
-def createScatterTime(dfWerbung,dfProgramm,columnName1,columnName2):
 
-
-    fig = plt.figure()
-    ax = fig.add_subplot(1,1,1)
-    ax.xaxis.set_major_locator(mdates.MinuteLocator(interval=60))
-    ax.xaxis.set_major_formatter(mdates.DateFormatter('%H'))
-    ax.scatter(dfWerbung[columnName1],dfWerbung[columnName2],color=COLOR_FOR_WERBUNG,label="Werbung",alpha=0.5,s=4)
-    ax.scatter(dfProgramm[columnName1],dfProgramm[columnName2],color=COLOR_FOR_PROGRAMM,label="Programm",alpha=0.5,s=4)
-    fig.autofmt_xdate()
-    # minute_locator = mdates.MinuteLocator(interval=240)
-    # hour_locator = mdates.HourLocator(interval=6)
-    # axis.xaxis.set_major_locator(minute_locator)
-    # myFmt = mdates.DateFormatter('%HH')
-    # axis.xaxis.set_major_formatter(myFmt)
-    # plt.scatter(dfWerbung[columnName1],dfWerbung[columnName2],color=COLOR_FOR_WERBUNG,label="Werbung",alpha=0.5,s=4)
-    # plt.scatter(dfProgramm[columnName1],dfProgramm[columnName2],color=COLOR_FOR_PROGRAMM,label="Programm",alpha=0.5,s=4)
-    ax.legend()
-    plt.show()
-
-def createScatterTimeY(dfWerbung,dfProgramm,columnName1,columnName2):
 
 
     fig = plt.figure()
     ax = fig.add_subplot(1,1,1)
-    ax.yaxis.set_major_locator(mdates.MinuteLocator(interval=60))
-    ax.yaxis.set_major_formatter(mdates.DateFormatter('%H'))
+  
     ax.scatter(dfWerbung[columnName1],dfWerbung[columnName2],color=COLOR_FOR_WERBUNG,label="Werbung",alpha=0.5,s=4)
     ax.scatter(dfProgramm[columnName1],dfProgramm[columnName2],color=COLOR_FOR_PROGRAMM,label="Programm",alpha=0.5,s=4)
     # minute_locator = mdates.MinuteLocator(interval=240)
@@ -219,22 +151,53 @@ def createScatterTimeY(dfWerbung,dfProgramm,columnName1,columnName2):
     ax.legend()
     plt.show()
 
-def createScatter(dfWerbung,dfProgramm,columnName1,columnName2):
-
-
-    fig = plt.figure()
-    ax = fig.add_subplot(1,1,1)
-    ax.scatter(dfWerbung[columnName1],dfWerbung[columnName2],color=COLOR_FOR_WERBUNG,label="Werbung",alpha=0.5,s=4)
-    ax.scatter(dfProgramm[columnName1],dfProgramm[columnName2],color=COLOR_FOR_PROGRAMM,label="Programm",alpha=0.5,s=4)
-    # minute_locator = mdates.MinuteLocator(interval=240)
-    # hour_locator = mdates.HourLocator(interval=6)
-    # axis.xaxis.set_major_locator(minute_locator)
-    # myFmt = mdates.DateFormatter('%HH')
-    # axis.xaxis.set_major_formatter(myFmt)
-    # plt.scatter(dfWerbung[columnName1],dfWerbung[columnName2],color=COLOR_FOR_WERBUNG,label="Werbung",alpha=0.5,s=4)
-    # plt.scatter(dfProgramm[columnName1],dfProgramm[columnName2],color=COLOR_FOR_PROGRAMM,label="Programm",alpha=0.5,s=4)
-    ax.legend()
+def createHeatmap(heatmap):
+    ##createHeatmap(specificDataFrame.corr())
+    plt.figure(figsize=(13, 6))
+    sns.heatmap(heatmap, vmax=1, annot=True, linewidths=.5)
+    plt.xticks(rotation=30, horizontalalignment="right")
     plt.show()
+
+def createScatters(dfWerbung,dfProgramm,columnName1,columnArray):
+    fig, axes = plt.subplots(len(columnArray)//4, 4, figsize=(12, 48))
+    cArr = columnArray.copy()
+    cArr.remove("LABEL")
+    if columnName1 in cArr: cArr.remove(columnName1)
+    counterForPlots = 0
+    for triaxis in axes:
+        for axis in triaxis:
+            if counterForPlots<len(cArr):
+                if columnName1 == "Zeit":
+                    axis.xaxis.set_major_locator(mdates.MinuteLocator(interval=120))
+                    axis.xaxis.set_major_formatter(mdates.DateFormatter('%H'))
+                    #axis.xticks(rotation = -45) # Rotates X-Axis Ticks by 45-degrees
+                elif cArr[counterForPlots] == "Zeit":
+                    axis.yaxis.set_major_locator(mdates.MinuteLocator(interval=120))
+                    axis.yaxis.set_major_formatter(mdates.DateFormatter('%H'))
+                    #axis.yticks(rotation = -45) # Rotates Y-Axis Ticks by 45-degrees
+                axis.set_xlabel(columnName1)
+                axis.set_ylabel(cArr[counterForPlots])
+                if counterForPlots==0:
+                    axis.scatter(dfWerbung[columnName1],dfWerbung[cArr[counterForPlots]],color=COLOR_FOR_WERBUNG,label="Werbung",alpha=ALPHA_VAL,s=4)
+                    axis.scatter(dfProgramm[columnName1],dfProgramm[cArr[counterForPlots]],color=COLOR_FOR_PROGRAMM,label="Programm",alpha=ALPHA_VAL,s=4)
+                else:
+                    axis.scatter(dfWerbung[columnName1],dfWerbung[cArr[counterForPlots]],color=COLOR_FOR_WERBUNG,alpha=ALPHA_VAL,s=4)
+                    axis.scatter(dfProgramm[columnName1],dfProgramm[cArr[counterForPlots]],color=COLOR_FOR_PROGRAMM,alpha=ALPHA_VAL,s=4) 
+
+            counterForPlots+=1
+    axis.legend()
+    manager=plt.get_current_fig_manager()
+    manager.full_screen_toggle()
+    fig.legend(loc='upper left')
+    fig.suptitle("Streungsdiagramme für "+columnName1, fontsize=14)
+    plt.show()
+
+## GET DATA FRAMES ##
+df = returnJoinedDataFrame(PATH_DATA_TO_BE_JOINED)
+df["Zeit"]=df["Zeit"].astype("datetime64[ns]")
+columnArray=["ECR_RATIO","MVL SUM","MVL ABS","RMS","DB","ZCR","MFCC","FARBWECHSEL RATIO","SIFT RATIO","Tag","Zeit","LABEL"]
+specificDataFrame = df[columnArray]
+
 ## REMOVE OUTLIERS AND FALSE NUMBERS ## 
 specificDataFrame = removeOutliers(specificDataFrame)
 
@@ -242,12 +205,15 @@ specificDataFrame = removeOutliers(specificDataFrame)
 dfWerbung = specificDataFrame[specificDataFrame['LABEL'] == "Werbung"]
 dfProgramm = specificDataFrame[specificDataFrame['LABEL'] == "Programm"]
 
-
-
 ## PRINT SUM Histograms ##
-createSumWerbungProgramHistograms([dfProgramm,dfWerbung],STATUSES)
+createSumWerbungProgramHistograms([dfProgramm,dfWerbung],STATUSES,columnArray)
+
+for x in columnArray:
+    createScatters(dfProgramm,dfWerbung,x,columnArray)
+
 #createHeatMaps
 #createDescriptions
+#createScatters
 
 
 #createHeatmap(specificDataFrame.corr())
